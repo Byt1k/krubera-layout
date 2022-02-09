dde5eb2d272dace99a15bc9f069bd83f6af82cda
Byt1k <byt1kip82@gmail.com>
2022-02-09 17:25:35 +0300

    Fixed mobile-menu

--- js/main.js
+++ js/main.js
@@ -2,28 +2,31 @@
 //   $(".anime-block").each(function () {
 //     var elPos = $(this).offset().top;
 //     var topOfWindow = $(window).scrollTop();
-//     if (elPos < topOfWindow + 1000) {
+//     if (elPos < topOfWindow + 700) {
 //       $(this).addClass("fadeInUp");
 //     }
 //   });
 // });
 
+let header = document.querySelector('header');
 let mobileMenuBtn = document.querySelector('.header__burger');
 let mobileMenu = document.querySelector('.header__menu');
 let bodyStyle = document.querySelector('body').style;
 
 mobileMenuBtn.addEventListener('click', () => {
   mobileMenu.classList.toggle('active');
+  header.classList.toggle('visible');
   if (mobileMenu.className == 'header__menu active') {
     bodyStyle.overflow = 'hidden';
   }
   else
-    bodyStyle.overflow = 'auto';
+    bodyStyle.overflow = 'visible';
 });
 
 [].forEach.call(mobileMenu.children, function (item) {
   item.addEventListener('click', () => {
     mobileMenu.classList.remove('active');
-    bodyStyle.overflow = 'auto';
+    bodyStyle.overflow = 'visible';
+    header.classList.remove('visible');
   })
 });
