f66b43b2797b2e9cb0b67530d091d3c85909a575
Byt1k <byt1kip82@gmail.com>
2022-02-09 17:17:37 +0300

    Added adaptive-version

--- js/main.js
+++ js/main.js
@@ -1,9 +1,29 @@
-$(window).scroll(function() {
-    $(".anime-block").each(function() {
-      var elPos = $(this).offset().top;
-      var topOfWindow = $(window).scrollTop();
-      if (elPos < topOfWindow + 700) {
-        $(this).addClass("fadeInUp");
-      }
-    });
-  });
+// $(window).scroll(function () {
+//   $(".anime-block").each(function () {
+//     var elPos = $(this).offset().top;
+//     var topOfWindow = $(window).scrollTop();
+//     if (elPos < topOfWindow + 1000) {
+//       $(this).addClass("fadeInUp");
+//     }
+//   });
+// });
+
+let mobileMenuBtn = document.querySelector('.header__burger');
+let mobileMenu = document.querySelector('.header__menu');
+let bodyStyle = document.querySelector('body').style;
+
+mobileMenuBtn.addEventListener('click', () => {
+  mobileMenu.classList.toggle('active');
+  if (mobileMenu.className == 'header__menu active') {
+    bodyStyle.overflow = 'hidden';
+  }
+  else
+    bodyStyle.overflow = 'auto';
+});
+
+[].forEach.call(mobileMenu.children, function (item) {
+  item.addEventListener('click', () => {
+    mobileMenu.classList.remove('active');
+    bodyStyle.overflow = 'auto';
+  })
+});
